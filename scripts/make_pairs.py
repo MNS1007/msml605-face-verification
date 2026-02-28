a9f29eb7280238a004aa1848885b64e46342d07b
Sriram Vema <74271047+sriramvema@users.noreply.github.com>
2026-02-28 18:07:13 -0500

    Update make_pairs.py

--- scripts/make_pairs.py
+++ scripts/make_pairs.py
@@ -2,12 +2,12 @@ import os
 import yaml
 import pandas as pd
 
-
+# Loads the config
 def load_config(config_path="config.yaml"):
     with open(config_path, "r") as f:
         return yaml.safe_load(f)
 
-
+# Makes the filepath for each persons image from your local cache
 def resolve_image_path(data_path, name, imagenum):
     filename = f"{name}_{int(imagenum):04d}.jpg"
     return os.path.join(data_path, "lfw-deepfunneled", "lfw-deepfunneled", name, filename)
@@ -15,7 +15,7 @@ def resolve_image_path(data_path, name, imagenum):
 
 def process_split(split_csv_path, split_name, data_path):
     df = pd.read_csv(split_csv_path)
-
+    # Use lambda function to apply the image path function to each name in each row
     df["left_path"] = df.apply(
         lambda row: resolve_image_path(data_path, row["name1"], row["imagenum1"]),
         axis=1
@@ -38,7 +38,7 @@ def main():
     seed = config["seed"]
 
     dataset_dir = "./dataset"
-    output_dir = "./dataset/pairs"
+    output_dir = "./dataset/pairs" # Creates a new folder inside dataset for the pairs
     os.makedirs(output_dir, exist_ok=True)
 
     splits = {
@@ -46,7 +46,7 @@ def main():
         "val": os.path.join(dataset_dir, "val.csv"),
         "test": os.path.join(dataset_dir, "test.csv"),
     }
-
+    # Creates the pairs for each split
     for split_name, split_csv_path in splits.items():
         pairs_df = process_split(split_csv_path, split_name, data_path)
         output_path = os.path.join(output_dir, f"{split_name}_pairs.csv")
